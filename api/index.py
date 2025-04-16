390286fab6e9f9b2e65bb1365821f61584e9fb90
hungnv2 <dntvh137@gmail.com>
2025-04-16 09:34:58 +0700

    fix: production cannot take picture

--- api/index.py
+++ api/index.py
@@ -18,7 +18,6 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("debug.log"),
         logging.StreamHandler()
     ]
 )
